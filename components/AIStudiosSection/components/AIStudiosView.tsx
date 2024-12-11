1e08f5431c77f147332f2e2094463808a79a38e4
gonzaarancibia <gonzaloarancibia48@gmail.com>
2024-12-11 13:31:58 +0100

    other: add log to debugging

--- components/AIStudiosSection/components/AIStudiosView.tsx
+++ components/AIStudiosSection/components/AIStudiosView.tsx
@@ -7,6 +7,8 @@ type Props = {
 
 const AIStudiosView = ({ AIStudios, error }: Props) => {
   if (error) {
+    console.log("error->", error);
+
     return (
       <section className="py-16 bg-gray-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
