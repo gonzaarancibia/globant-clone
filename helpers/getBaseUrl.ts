09209f37f5585dd9f8d8d1a05e3cc8c2a8937c93
gonzaarancibia <gonzaloarancibia48@gmail.com>
2024-12-11 13:44:05 +0100

    fix: base domain in requests

--- helpers/getBaseUrl.ts
+++ helpers/getBaseUrl.ts
@@ -1,5 +1,7 @@
 export function getBaseUrl() {
-  return !!process.env.VERCEL_ENV
-    ? `https://${process.env.VERCEL_URL}`
+  return process.env.VERCEL_ENV === "production" ||
+    process.env.VERCEL_ENV === "preview" ||
+    process.env.VERCEL_ENV === "development"
+    ? process.env.PRODUCTION_API_DOMAIN
     : process.env.NEXT_PUBLIC_API_URL;
 }
